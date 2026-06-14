8d61910544655504667a0b4ac8f7a796bafcc74f
Puleeno Nguyen <puleeno@gmail.com>
2026-06-14 21:31:31 +0700

    feat: enable admin authentication by default and remove homepage route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,4 @@ $router->get('/dashboard', \App\Http\Controllers\Admin\SpaController::class);
 $router->get('/api/admin/menu', [\App\Http\Controllers\Admin\SpaController::class, 'menu']);
 $router->get('/api/admin/dashboard/widgets', [\App\Http\Controllers\Admin\SpaController::class, 'dashboardWidgets']);
 
-// Homepage
-$router->get('/', function () {
-    return \Witals\Framework\Http\Response::html('<h1>Hello, PrestoWorld!</h1><p><a href="/login">Login</a> | <a href="/dashboard">Dashboard</a></p>');
-});
+
